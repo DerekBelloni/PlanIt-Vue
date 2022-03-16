b321441704d7c78ea0191287a1df46fff8e80401
Derek <derekbelloni10@gmail.com>
2022-03-16 14:28:01 -0600

    commit

---
+++ PlanIt-Vue.client/src/components/CreateSprintForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="component">
+    <form class="d-flex flex-column">
+      <label for="">Project Name</label>
+      <input
+        v-model="editable.name"
+        type="text"
+        placeholder="project name..."
+      />
+      <label for="">Project Description</label>
+      <input
+        v-model="editable.description"
+        type="text"
+        placeholder="project description..."
+      />
+      <button class="btn btn-primary m-2" @click="createProject()">
+        Submit
+      </button>
+    </form>
+  </div>
+</template>
+
+
+<script>
+export default {
+  setup() {
+
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+</style>
